41f93239ec8170cbb53d06fbba8f95eb7f20eaad
XZ <xilan.zhang01@gmail.com>
2025-01-31 15:09:46 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ Integrated with Additional Indicators from balance_group_check.py
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 import requests
